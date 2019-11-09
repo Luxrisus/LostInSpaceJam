491e63237e6d49e412d0b713c1f64aff8bb0200a
Julien Balestrieri <julien.balestrieri@gmail.com>
2019-11-09 20:23:04 +0100

    Clean up | base for links

---
+++ JamLea20191108/Assets/Scripts/Link.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Link : MonoBehaviour
+{
+    private LineRenderer _lineRenderer;
+
+    void Start()
+    {
+        _lineRenderer = GetComponent<LineRenderer>();
+        UpdatePosition(Vector3.zero, Vector3.zero);
+    }
+    
+    public void SetStart(Vector2 start)
+    {
+        _lineRenderer.SetPosition(0, new Vector3(start.x, start.y, 0f));
+    }
+    
+    public void SetEnd(Vector2 end)
+    {
+        _lineRenderer.SetPosition(1, new Vector3(end.x, end.y, 0f));
+    }
+
+    public void UpdatePosition(Vector2 start, Vector2 end)
+    {
+        SetStart(start);
+        SetEnd(end);
+    }
+
+    public float GetDistance()
+    {
+        return Vector3.Distance(_lineRenderer.GetPosition(0), _lineRenderer.GetPosition(1));
+    }
+}
---
+++ JamLea20191108/Assets/Scripts/Linker.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ILinkable
+{
+    void OnLinkadded();
+    void OnLinkDestroyed();
+    Vector3 GetPosition();
+
+    //bool CanMove(); apply restrictions based on distance and max count
+}
+
+public class Linker : MonoBehaviour, ILinkable
+{
+    [SerializeField]
+    private Link _linkPrefab;
+
+    [SerializeField]
+    int _maxLink = 3;
+    
+    [SerializeField]
+    float _maxDistance = 10f;
+    
+    public struct LinkData
+    {
+        public ILinkable LinkStart;
+        public ILinkable LinkEnd;
+        public Link LinkObject;
+        
+        public LinkData(ILinkable linkStart, ILinkable linkEnd, Link linkObject)
+        {
+            LinkStart = linkStart;
+            LinkEnd = linkEnd;
+            LinkObject = linkObject;
+
+        }
+
+        public void Update()
+        {
+            LinkObject.UpdatePosition(LinkStart.GetPosition(), LinkEnd.GetPosition());
+        }
+    }
+
+    List<LinkData> _links = new List<LinkData>();
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        foreach (LinkData linkData in _links)
+        {
+            linkData.Update();
+        }
+    }
+    
+    public void AddLink(ILinkable linkable)
+    {
+        Link link = Instantiate(_linkPrefab);
+
+        _links.Add(new LinkData(this, linkable, link));
+    }
+    
+    public void RemoveLink(ILinkable linkable)
+    {
+        //todo
+        _links.RemoveAll(l => (l.LinkStart == linkable || l.LinkEnd == linkable));
+    }
+
+    public void OnLinkadded()
+    {
+
+    }
+
+    public void OnLinkDestroyed()
+    {
+
+    }
+
+    public Vector3 GetPosition()
+    {
+        return transform.position;
+    }
+}
