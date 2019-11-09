09707d85d3b47a3e5968dc399103344ee80ae3e4
Julien Balestrieri <julien.balestrieri@gmail.com>
2019-11-09 19:42:29 +0100

    Add player input

---
+++ JamLea20191108/Assets/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 5f;
+
+    Vector3 _direction = Vector3.zero;
+
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        Gamepad gamepad = Gamepad.current;
+        if (gamepad != null)
+        {
+            if (gamepad.rightTrigger.wasPressedThisFrame)
+            {
+                Debug.Log("Fire test");
+            }
+
+            transform.Translate(_speed * Time.deltaTime * _direction);
+        }
+    }
+
+    public void OnMove(InputValue value)
+    {
+        Vector2 move = value.Get<Vector2>();
+        _direction = new Vector3(move.x, move.y, 0f);
+    }
+}
