a5554c36abf836353120e12c5bbd8d4efefdebe4
Lea Bouvier-Chenard <leabouvier@free.fr>
2019-11-10 20:26:30 +0100

    craftable elements

---
+++ JamLea20191108/Assets/Scripts/Component/CraftableComponent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraftableComponent : ATransportableElement
+    {
+
+    [SerializeField]
+    private IngredientDisplay _ingredientDisplay;
+    [SerializeField]
+    private SpriteRenderer _sprite;
+
+    private void Awake()
+    {
+        this._sprite.sprite = _ingredientDisplay.Sprite;
+    }
+}
