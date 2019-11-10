83ff59165ce25424789052255901f4a8a57f870e
Julien Balestrieri <julien.balestrieri@gmail.com>
2019-11-10 23:22:40 +0100

    Add menu

---
+++ JamLea20191108/Assets/Scripts/UI/MenuController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MenuController : MonoBehaviour
+{
+    List<MenuEntry> _entries = new List<MenuEntry>();
+    int _index = 0;
+
+    void Start()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            MenuEntry entry = transform.GetChild(i).gameObject.GetComponent<MenuEntry>();
+            if (entry != null)
+            {
+                _entries.Add(entry);
+            }
+        }
+        _entries[_index].Select();
+    }
+
+    public void OnNavigate(InputValue value)
+    {
+        Vector2 direction = value.Get<Vector2>();
+        Debug.Log("value" + direction);
+
+        int prevIndex = _index;
+        _index -= (int)direction.y;
+        if (_index >= _entries.Count)
+        {
+            _index = 0;
+        }
+        else if (_index < 0)
+        {
+            _index = _entries.Count - 1;
+        }
+        
+        _entries[prevIndex].Unselect();
+        _entries[_index].Select();
+    }
+
+    public void OnSubmit()
+    {
+        _entries[_index].DoAction();
+    }
+}
---
+++ JamLea20191108/Assets/Scripts/UI/MenuEntry.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuEntry : MonoBehaviour
+{
+    [SerializeField]
+    private Color _selectedColor = Color.red;
+    
+    [SerializeField]
+    private string _sceneName;
+
+    private Color _currentColor;
+
+    void Start()
+    {
+        
+    }
+
+    public void Select()
+    {
+        _currentColor = GetComponent<UnityEngine.UI.Image>().color;
+        GetComponent<UnityEngine.UI.Image>().color = _selectedColor;
+    }
+
+    public void Unselect()
+    {
+        GetComponent<UnityEngine.UI.Image>().color = _currentColor;
+    }
+
+    public void DoAction()
+    {
+        SceneManager.LoadScene(_sceneName, LoadSceneMode.Single);
+    }
+}
