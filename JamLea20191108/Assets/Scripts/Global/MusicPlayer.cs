c00b7cfd546ecb9e50ea2fa6011bdd39b99e087d
Lea Bouvier-Chenard <leabouvier@free.fr>
2019-11-19 21:26:13 -0500

    add main music

---
+++ JamLea20191108/Assets/Scripts/Global/MusicPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    private AudioSource _audioSource;
+    
+    [SerializeField]
+    private AudioClip _audioClip;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        _audioSource = gameObject.AddComponent<AudioSource>();
+        _audioSource.clip = _audioClip;
+        _audioSource.loop = true;
+        _audioSource.Play();
+
+        SceneManager.LoadScene(1);
+    }
+
+}
